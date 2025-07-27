2b35c8aa947771aac9a7ee9e1f91f000bf0d67b8
vinicenter <viniemidiobosi@gmail.com>
2025-07-27 00:34:06 -0300

    fix: change fallback service port to 443 and update docker compose command to run in detached mode

--- gateway/fallback/app.py
+++ gateway/fallback/app.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = '0.0.0.0'
-PORT = 4444
+PORT = 443
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
--- gateway/startGateway.sh
+++ gateway/startGateway.sh
@@ -3,4 +3,4 @@
 source ./.env
 
 DESTINATION_IP=$DESTINATION_IP GATEWAY_HOST=$GATEWAY_HOST envsubst '${DESTINATION_IP} ${GATEWAY_HOST}' < ./nginx/nginx.conf.template > ./nginx/nginx.conf
-docker compose up
+docker compose up -d
