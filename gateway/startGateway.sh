4c158556ffdf08646b23b20f1475d1afdc979256
vinicenter <viniemidiobosi@gmail.com>
2025-07-27 00:13:05 -0300

    feat: add nginx configuration setup script

---
+++ gateway/startGateway.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+source ./.env
+
+DESTINATION_IP=$DESTINATION_IP GATEWAY_HOST=$GATEWAY_HOST envsubst '${DESTINATION_IP} ${GATEWAY_HOST}' < ./nginx/nginx.conf.template > ./nginx/nginx.conf
+docker compose up
